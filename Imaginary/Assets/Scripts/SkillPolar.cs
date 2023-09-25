95809f8111b0aceccda387734cf93825f3d6da0c
Yutong Yi <YiAndrew2001@gmail.com>
2023-09-25 15:08:24 -0400

    Implemented Polar stage 1 LineRenderer

--- Imaginary/Assets/Scripts/SkillPolar.cs
+++ Imaginary/Assets/Scripts/SkillPolar.cs
@@ -8,19 +8,30 @@ public class SkillPolar : MonoBehaviour
     // Variables
     // Components
     private Rigidbody2D rb;
-    // 
+    private LineRenderer lr1;
+
+    // Polar
     private bool isInPolar1 = false;
     private bool isInPolar2 = false;
+
+    [SerializeField] private GameObject lrPolar1GameObject;
+    private Vector3[] points1 = new Vector3[2];
+
 
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        lr1 = lrPolar1GameObject.GetComponent<LineRenderer>();
     }
 
     void Update()
     {
-        
+        if (isInPolar1)
+        {
+            points1[1] = transform.position;
+            drawLinePolar1();
+        }
     }
 
 
@@ -28,22 +39,28 @@ public class SkillPolar : MonoBehaviour
     // Input System
     public void Polar(InputAction.CallbackContext context)
     {
-        if (!IsInPolar())
+        if (context.performed)
         {
-            // Not in Polar mode: enter Polar mode
-            isInPolar1 = true;
-            rb.constraints = RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezeRotation;
-        }
-        else if (IsInPolar1())
-        {
-            // In Polar stage 1: exit Polar mode
-            isInPolar1 = false;
-            rb.constraints = RigidbodyConstraints2D.FreezeRotation;
-        }
-        else
-        {
-            // In Polar stage 2: exit Polar mode
-            isInPolar2 = false;
+            if (!IsInPolar())
+            {
+                // Not in Polar mode: enter Polar mode
+                isInPolar1 = true;
+                rb.constraints = RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezeRotation;
+                lr1.positionCount = 2;
+                points1[0] = transform.position;
+            }
+            else if (isInPolar1)
+            {
+                // In Polar stage 1: exit Polar mode
+                isInPolar1 = false;
+                rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+                lr1.positionCount = 0;
+            }
+            else
+            {
+                // In Polar stage 2: exit Polar mode
+                isInPolar2 = false;
+            }
         }
     }
 
@@ -62,6 +79,12 @@ public class SkillPolar : MonoBehaviour
 
     public bool IsInPolar()
     {
-        return IsInPolar1() || IsInPolar2();
+        return isInPolar1 | isInPolar2;
+    }
+
+    private void drawLinePolar1()
+    {
+        lr1.SetPosition(0, points1[0]);
+        lr1.SetPosition(1, points1[1]);
     }
 }
